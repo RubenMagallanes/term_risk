fa67a06bfc12948517e3cdffa63c64ca07b2b5dc
RubenMagallanes <rubenmmm3@gmail.com>
2017-06-03 22:55:07 +1200

    add territory class

---
+++ risk_territory.py
@@ -0,0 +1,22 @@
+class Territory(object):
+	#name - string - territory name
+	#adjacent - list[string] - list of other territory names
+	
+	#owner - string - name of player owns this territory
+	#troops - int - number of troops in this territory
+
+	def __init__(self, tname, next2):
+		self.name = tname
+		self.adjacent = next2
+		self.owner = "" # starts empty
+		self.troops = 0
+
+	def add_troops(self, num):
+		self.troops += num
+
+	def remove_troops(self, num):
+		self.troops -= num
+
+	def change_ownership(self, newplayer):
+		self.owner = newplayer
+
