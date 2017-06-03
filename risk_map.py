5629d6dd4ab3e9e924949c9f77cf06b6bcadffff
RubenMagallanes <rubenmmm3@gmail.com>
2017-06-03 23:57:56 +1200

    change map constructor to read from file

--- risk_map.py
+++ risk_map.py
@@ -1,11 +1,23 @@
+from risk_territory import Territory
+
 class Map(object):
 	#territories - list of territory objects
 	
-	def __init__(self):
+	def __init__(self, fname):
 		self.territories = []
 		#self.continents = [] TODO
-	
-	def add_territory(self, territory):
-		self.territories.append(territory)
+		
+		lines = []
+		with open(fname) as f:
+			lines = f.read().split('\n')
 
+		t_name = ''
+		territory = ''
+		for en in lines:
+			cmd = en.split(':')
+			if cmd[0] == 'n':
+				t_name = cmd[1]
+			if cmd[0] == 'a':
+				territory = Territory(t_name, [cmd[1]])
+				self.territories.append(territory)
 
