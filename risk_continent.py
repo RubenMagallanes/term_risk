5996909ad747ed897be6c3c20835229a78614088
RubenMagallanes <rubenmmm3@gmail.com>
2017-06-06 18:31:46 +1200

    create continent class

---
+++ risk_continent.py
@@ -0,0 +1,12 @@
+def Continent(object):
+	#name - string - name of this continent
+	#value - int - bonus for controlling this continent at start of turn
+	#territories - [str] - territories that make up this continent
+	def __init__(self, cname, cvalue, t_list):
+		self.name = cname
+		self.value = cvaue
+		self.territories = t_list
+	
+	def territory_list(self):
+		return territories
+
